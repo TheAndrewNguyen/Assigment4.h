f11304622bbee9a6cea44d8e1b66419ffb4a4e5f
winnsta <158104929+winnsta@users.noreply.github.com>
2025-05-22 17:30:30 -0700

    Update main.cpp
    Added code for our main.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+    shhh libManager = new libManager();
+    libManager.insertPub(data file 1);
+    libManager.insertClient(data file 2);
+    libManager.getCommands(data file 3);
+    
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
